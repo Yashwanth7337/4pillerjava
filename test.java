78ff8c66e2d6a4b34570ccc9a51d71ecfcabb911
yashwanth7337 <yash.yashwanth123@gmail.com>
2023-07-04 13:15:40 +0530

    hi

---
+++ test.java
@@ -0,0 +1,18 @@
+class art extends test{
+
+}
+
+
+public class test {
+   public static void main(String[] args) {
+    String s1=new String ("madhu");  //String obj method
+    String s2=new String("madhu");
+    String s3=new String("sai");
+    int s4=  (10);
+    String s5="yeshwanth";
+    System.out.println(s1.equals(s2));
+    System.out.println(s1.equals(s3));
+    System.out.println(s1.equals(s5));
+
+   } 
+}
