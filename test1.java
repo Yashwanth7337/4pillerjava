7db52b7e920faa77747e1dacd6a2be9d7a2c701c
yashwanth7337 <yash.yashwanth123@gmail.com>
2023-07-04 13:15:28 +0530

    hi

---
+++ test1.java
@@ -0,0 +1,12 @@
+public class test1 {
+    public static void main(String[] args) {
+    test t2=new test();
+    int a=10;
+    int b=20;
+    int c=10;
+    System.out.println(a==b);   //primitive data type
+    System.out.println(a==c);
+    System.out.println(c==b);
+}
+    
+}
